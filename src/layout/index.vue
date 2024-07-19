af5ff1f6a340e8afa37b9cc0567c5d94901e235c
okay <138407783+okay2333@users.noreply.github.com>
2024-07-19 19:05:59 +0800

    修复侧边栏bug

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,6 +1,6 @@
 <template>
   <a-layout>
-    <a-layout-sider :style="{ height: '100vh' }" theme="dark" collapsible><Sider /></a-layout-sider>
+    <a-layout-sider theme="dark" collapsible><Sider /></a-layout-sider>
     <a-layout>
       <a-layout-header :style="{ backgroundColor: '#FFFFFF' }"><Header /></a-layout-header>
       <a-layout-content><router-view /></a-layout-content>
