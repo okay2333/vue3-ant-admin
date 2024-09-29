6c21a0c1236106c278a8ff20c3ac2aeb1b4b4094
okay <138407783+okay2333@users.noreply.github.com>
2024-09-29 08:38:49 +0800

    完成路由和侧边栏

--- src/layout/sider/index.vue
+++ src/layout/sider/index.vue
@@ -11,17 +11,30 @@ const router = useRouter()
 const routes = computed(() => {
   return generateMenus(filterRouters(router.getRoutes()))
 })
+import icon from '@/components/icon.vue'
+console.log('过滤后的路由', routes.value)
 </script>
 
 <template>
   <a-menu :open-keys="openKeys" @update:openKeys="handleOpenKeysChange" mode="inline" theme="dark">
     <template v-for="route in routes">
-      <a-sub-menu v-if="route.children.length" :key="route.path" :title="route.meta.title">
-        <a-menu-item v-for="child in route.children" :key="child.path" :title="child.meta.title">
-          <router-link :to="child.path">{{ child.meta.title }}</router-link>
+      <a-sub-menu v-if="route.children.length" :key="route.path">
+        <template #title>
+          <icon
+            :icon-name="route.meta.icon"
+            :style="{ color: '#FFFFF', fontSize: '15px' }"
+            class="item-icon"
+          />
+          {{ route.meta.title }}
+        </template>
+        <a-menu-item v-for="child in route.children" :key="child.path">
+          <icon :icon-name="child.meta.icon" :style="{ color: '#FFFFF' }" />
+          <router-link :to="child.path">{{ child.meta.icon }}</router-link>
         </a-menu-item>
       </a-sub-menu>
+      <!-- 只有一个节点 -->
       <a-menu-item v-else :key="route.path as string">
+        <icon :icon-name="route.meta.icon" :style="{ color: '#FFFFF' }" />
         <router-link :to="route.path">{{ route.meta.title }}</router-link>
       </a-menu-item>
     </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -119,7 +119,7 @@ const publicRoutes = [
         component: () => import('@/views/profile/index.vue'),
         meta: {
           title: 'profile',
-          icon: 'el-icon-user'
+          icon: 'role'
         }
       },
       {
